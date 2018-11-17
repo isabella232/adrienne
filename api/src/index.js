1dc36c903742a8fbab30f9f58f59e2d2c74b7402
sergiomcpereira <sergiomcpereira@gmail.com>
2018-11-17 18:49:41 +0000

    added baseline data and updated  endpoints

--- api/src/index.js
+++ api/src/index.js
@@ -52,6 +52,8 @@ function initDatabase() {
   mongoDB.db(databaseName).collection("Vehicles").insertMany([
      // MongoDB adds the _id field with an ObjectId if _id is not present
      //Demo: Gullwing, Focus for Family, autonomous electic, truck, bus
+
+     //classic cars
      { id: "1",
       type: "6",
       picture: "https://rmsothebys-cache.azureedge.net/2/4/1/5/6/c/24156c38dc4e4072bc656f5217183a1c56b16014.jpg",
@@ -123,6 +125,7 @@ function initDatabase() {
         ]
       },
 
+      //family cars
       { id: "4",
        type: "4",
        picture: "https://images.caricos.com/f/ford/2015_ford_focus_wagon/images/1920x1080/2015_ford_focus_wagon_1_1920x1080.jpg",
@@ -191,7 +194,142 @@ function initDatabase() {
            {serviceType: "Insurance", serviceProvider: "Fidelidade", serviceFee: "6.89", serviceDate: "134"},
            {serviceType: "Repair", serviceProvider: "Midas", serviceFee: "152.21", serviceDate: "538"}
          ]
-       }
+       },
+
+       //autonomous cars
+       { id: "7",
+        type: "2",
+        picture: "https://cdn.vox-cdn.com/uploads/chorus_image/image/50500495/2120x920_MS-RedSunset.0.png",
+        make: "Model S",
+        model: "Tesla",
+        date: "2016",
+        doors: "5",
+        fuel: "Electric",
+        driving: "Autonomous",
+        price: "310",
+        co2: "0",
+        shildSeat: "no",
+        services:
+          [
+            {serviceType: "Electric Recharging", serviceProvider: "Telsa Charging Station", serviceFee: "13.21", serviceDate: "4"},
+            {serviceType: "Interior Cleaning", serviceProvider: "Tesla Lovers Garage", serviceFee: "18", serviceDate: "15"},
+            {serviceType: "Washing", serviceProvider: "Tesla Lovers Garage", serviceFee: "18", serviceDate: "25"},
+            {serviceType: "Insurance", serviceProvider: "Allianz", serviceFee: "6.89", serviceDate: "90"},
+            {serviceType: "Repair", serviceProvider: "Tesla Lovers Garage", serviceFee: "152.21", serviceDate: "538"}
+          ]
+        },
+      { id: "8",
+       type: "2",
+       picture: "https://www.telegraph.co.uk/cars/images/2017/02/20/2017_PRIUS_PHV_Aqua_Dynamic_04_trans_NvBQzQNjv4BquEFCvHreRVioWItRnm1hZbdxjchbeOcGRrsdcUP4AQs.jpg?imwidth=450",
+       make: "Toyota",
+       model: "Prius",
+       date: "2017",
+       doors: "5",
+       fuel: "Electric",
+       driving: "Autonomous",
+       price:"220",
+       co2: "0",
+       shildSeat: "no",
+       services:
+         [
+           {serviceType: "Electric Recharging", serviceProvider: "Repsol", serviceFee: "53.78", serviceDate: "36"},
+           {serviceType: "Interior Cleaning", serviceProvider: "Midas", serviceFee: "18", serviceDate: "15"},
+           {serviceType: "Washing", serviceProvider: "Classic Care", serviceFee: "18", serviceDate: "25"},
+           {serviceType: "Insurance", serviceProvider: "Allianz", serviceFee: "6.89", serviceDate: "134"},
+           {serviceType: "Repair", serviceProvider: "Ford Workshop", serviceFee: "152.21", serviceDate: "538"}
+         ]
+       },
+       { id: "10",
+        type: "9",
+        picture: "https://cdn1.autoexpress.co.uk/sites/autoexpressuk/files/2016/09/c-350-e-fd-23.jpg",
+        make: "Mercedes",
+        model: "C Class 350e",
+        date: "2018",
+        doors: "5",
+        fuel: "Electric",
+        driving: "Autonomous",
+        price:"290",
+        co2: "0",
+        shildSeat: "no",
+        services:
+          [
+            {serviceType: "Parking", serviceProvider: "Martim Moniz Park", serviceFee: "18.75", serviceDate: "1"},
+            {serviceType: "Interior Cleaning", serviceProvider: "Norauto", serviceFee: "18", serviceDate: "15"},
+            {serviceType: "Washing / Polishing", serviceProvider: "Classic Care", serviceFee: "18", serviceDate: "18"},
+            {serviceType: "Electric Recharging", serviceProvider: "Galp", serviceFee: "53.78", serviceDate: "36"},
+            {serviceType: "Insurance", serviceProvider: "Fidelidade", serviceFee: "6.89", serviceDate: "134"},
+            {serviceType: "Repair", serviceProvider: "Midas", serviceFee: "152.21", serviceDate: "538"}
+          ]
+        },
+
+        //trucks
+        { id: "11",
+         type: "9",
+         picture: "https://www.coches.com/fotos_historicas/mercedes/Vito-Mixto-W639-2014/high_mercedes_vito-mixto-w639-2014_r6.jpg",
+         make: "Mercedes",
+         model: "Vito",
+         date: "2014",
+         doors: "5",
+         fuel: "Diesel",
+         driving: "Manual",
+         price: "210",
+         co2: "200",
+         shildSeat: "no",
+         services:
+           [
+             {serviceType: "Delivery", serviceProvider: "Serafim Ribeiro", serviceFee: "13.21", serviceDate: "4"},
+             {serviceType: "Petrol Refuel", serviceProvider: "Telsa Charging Station", serviceFee: "13.21", serviceDate: "4"},
+             {serviceType: "Interior Cleaning", serviceProvider: "Tesla Lovers Garage", serviceFee: "18", serviceDate: "15"},
+             {serviceType: "Washing", serviceProvider: "Tesla Lovers Garage", serviceFee: "18", serviceDate: "25"},
+             {serviceType: "Insurance", serviceProvider: "Allianz", serviceFee: "6.89", serviceDate: "90"},
+             {serviceType: "Repair", serviceProvider: "Tesla Lovers Garage", serviceFee: "152.21", serviceDate: "538"}
+           ]
+         },
+       { id: "12",
+        type: "9",
+        picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nissan_NV200_Vanette_Van_001.JPG/1200px-Nissan_NV200_Vanette_Van_001.JPG",
+        make: "Nissan",
+        model: "NV200",
+        date: "2016",
+        doors: "5",
+        fuel: "Diesel",
+        driving: "Manual",
+        price:"200",
+        co2: "180",
+        shildSeat: "no",
+        services:
+          [
+            {serviceType: "Pick Up", serviceProvider: "Garagem Joaquim Ribeiro", serviceFee: "48.89", serviceDate: "0"},
+            {serviceType: "Petrol Refuel", serviceProvider: "Repsol", serviceFee: "53.78", serviceDate: "12"},
+            {serviceType: "Interior Cleaning", serviceProvider: "Midas", serviceFee: "18", serviceDate: "15"},
+            {serviceType: "Washing", serviceProvider: "Classic Care", serviceFee: "18", serviceDate: "25"},
+            {serviceType: "Insurance", serviceProvider: "Allianz", serviceFee: "6.89", serviceDate: "134"},
+            {serviceType: "Repair", serviceProvider: "Ford Workshop", serviceFee: "152.21", serviceDate: "538"}
+          ]
+        },
+        { id: "9",
+         type: "2",
+         picture: "http://images.autojini.net/AJC/50424/vehicles/4609536_395844E9-E95A-0355-D03475840352D4C2_x.jpg",
+         make: "Ford",
+         model: "Transit",
+         date: "2018",
+         doors: "5",
+         fuel: "Diesel",
+         driving: "Manual",
+         price:"290",
+         co2: "170",
+         shildSeat: "no",
+         services:
+           [
+             {serviceType: "Delivery", serviceProvider: "Manuel Joaquim", serviceFee: "24.50", serviceDate: "0"},
+             {serviceType: "Parking", serviceProvider: "Martim Moniz Park", serviceFee: "18.75", serviceDate: "1"},
+             {serviceType: "Interior Cleaning", serviceProvider: "Norauto", serviceFee: "18", serviceDate: "15"},
+             {serviceType: "Washing / Polishing", serviceProvider: "Classic Care", serviceFee: "18", serviceDate: "18"},
+             {serviceType: "Petrol Refuel", serviceProvider: "Galp", serviceFee: "53.78", serviceDate: "36"},
+             {serviceType: "Insurance", serviceProvider: "Fidelidade", serviceFee: "6.89", serviceDate: "134"},
+             {serviceType: "Repair", serviceProvider: "Midas", serviceFee: "152.21", serviceDate: "538"}
+           ]
+         }
   ]);
   //mongoDB.close();
 }
@@ -250,8 +388,8 @@ app.get('/service-selected', (request, response) => {
  * return an array of {int, string} representing {id, description}
  */
 app.get('/search-vehicles', (request, response) => {
-  //request.
-  mongoDB.db(databaseName).collection("Vehicles").find().toArray(function(err, docs) {
+  var type = request.param("type");
+  mongoDB.db(databaseName).collection("Vehicles").find({"type" : type}).toArray(function(err, docs) {
     var data = docs;
     response.send(data);
   });
